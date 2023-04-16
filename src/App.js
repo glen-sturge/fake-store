a4ac4a456807a04bce212e5f2044c9ebf8ec7db1
Laura Fry <laura.fry@mun.ca>
2023-04-16 13:31:51 -0230

    Components created for support and contact page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import ProductGrid from "./components/ProductGrid";
 import Products from "./components/Products";
 import SaleBanner from "./components/SaleBanner";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Contact from "./components/Contact";
+import Support from "./components/Support";
 
 function App() {
   return (
@@ -23,6 +25,8 @@ function App() {
             }
           />
           <Route path="/shop" element={<Products />} />
+          <Route path="/support" element={<Support />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         {/* <Footer/> */}
       </div>
