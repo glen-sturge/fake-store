869312149295f31edd6e3ca943d5807b3ae695ff
Glen Sturge <glen.sturge@keyin.com>
2023-04-18 21:43:55 -0230

    product-select added

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,48 +1,52 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Product from "./Product";
 import { useState } from "react";
 import { useEffect } from "react";
 
 const ProductDetails = ({ product }) => {
   return (
-    <div className="product-details">
-      <div className="back-link">&#60; Back</div>
-      <div className="product-title2">{product.title}</div>
-      <br />
-      <div className="product-image-container">
-        <img src={product.image} alt="Product" width="300px" />
-      </div>
-      <p className="product-detail-description">{product.description}</p>
-      <div className="purchase-container">
-        <div className="size-quantity-wrapper">
-          <div className="size-container">
-            <div className="size">Size:</div>
-            {/* <div className="dropdown-size"> */}
-            <select className="quantity-select">
-              <option className="quantity-option">Extra-Small</option>
-              <option className="quantity-option">Small</option>
-              <option className="quantity-option">Medium</option>
-              <option className="quantity-option">Large</option>
-              <option className="quantity-option">Extra-Large</option>
-            </select>
-            {/* </div> */}
-          </div>
-          <div className="size-container">
-            <div className="size">Quantity:</div>
-            {/* <div className="quantity-picker"> */}
-            <input
-              type={"number"}
-              placeholder="1"
-              min={"1"}
-              max={"10"}
-              className="product-quantity"
-            ></input>
-            {/* </div> */}
-          </div>
+    <div className="product-details-wrapper">
+      <div className="product-details">
+        <div className="back-link">
+          <Link to="/shop">&#60; Back</Link>
         </div>
-        <div className="product-details-price">$9.99</div>
-        <div className="add-cart-btn-container">
-          <button className="add-cart-btn">Add to Cart</button>
+        <div className="product-title2">{product.title}</div>
+        <div className="product-image-container">
+          <img src={product.image} alt="Product" width="300px" />
+        </div>
+        <p className="product-detail-description">{product.description}</p>
+        <div className="purchase-container">
+          <div className="size-quantity-wrapper">
+            <div className="size-container">
+              <div className="size">Size:</div>
+              {/* <div className="dropdown-size"> */}
+              <select className="size-select">
+                <option className="size-option">Extra-Small</option>
+                <option className="size-option">Small</option>
+                <option className="size-option">Medium</option>
+                <option className="size-option">Large</option>
+                <option className="size-option">Extra-Large</option>
+              </select>
+              {/* </div> */}
+            </div>
+            <div className="size-container">
+              <div className="size">Quantity:</div>
+              <input
+                type={"number"}
+                placeholder="1"
+                min={"1"}
+                max={"10"}
+                className="product-quantity"
+              ></input>
+            </div>
+          </div>
+          <div className="product-details-price">
+            ${product.price.toFixed(2)}
+          </div>
+          <div className="add-cart-btn-container">
+            <button className="add-cart-btn">Add to Cart</button>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,12 +2,29 @@ import React from "react";
 import Product from "./Product";
 import "../App.css";
 
-const Products = ({ products, setProduct }) => {
+const Products = ({ products, setProduct, setCategory }) => {
+  const handleChange = (e) => {
+    // console.log(e.target.value);
+    setCategory(e.target.value);
+  };
+
   return (
-    <div className="products-gallery">
-      {products.map((product) => (
-        <Product key={product.id} setProduct={setProduct} product={product} />
-      ))}
+    <div className="products-gallery-wrapper">
+      <div className="products-gallery">
+        <div className="products-select">
+          <select className="select-category" onChange={handleChange}>
+            <option value="all">- All -</option>
+            <option value="electronics">- Electronics -</option>
+            <option value="jewelery">- Jewelry -</option>
+            <option value="women's clothing">- Women's clothing -</option>
+            <option value="men's clothing">- Men's clothing -</option>
+          </select>
+        </div>
+
+        {products.map((product) => (
+          <Product key={product.id} setProduct={setProduct} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
